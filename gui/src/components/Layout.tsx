38f18484d6f0d702af09b12f8ea82bafe4509497
Nang <nathanang2000@gmail.com>
2024-08-02 11:26:26 -0700

    Added Add selected (#119)

--- gui/src/components/Layout.tsx
+++ gui/src/components/Layout.tsx
@@ -177,7 +177,7 @@ const shortcuts = [
   { modifiers: [modifier], keyCode: '0', description: 'Prev', onClick: () => postToIde('lastChat', undefined) },
   { modifiers: [modifier], keyCode: 'O', description: 'History' },
   { modifiers: [modifier], keyCode: ';', description: 'Close', onClick: () => postToIde('closeChat', undefined) },
-  { modifiers: [modifier, 'Shift'], keyCode: 'L', description: 'Add' },
+  { modifiers: [modifier, 'Shift'], keyCode: 'L', description: 'Add Selected' },
 ];
 
 
