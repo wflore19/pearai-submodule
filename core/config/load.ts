5b655ec996bdfdc94b08629f8b07f56dc13c10f5
ICEPrey <40725969+ICEPrey@users.noreply.github.com>
2024-08-23 16:32:31 -0700

    feat: Login Button (Status Bar) (#150)
    Co-authored-by: Nang <nathanang2000@gmail.com>

--- core/config/load.ts
+++ core/config/load.ts
@@ -546,15 +546,18 @@ function getTarget() {
 }
 
 function escapeSpacesInPath(p: string): string {
-  return p.split("").map(char => {
-    if (char === " ") {
-      return "\\ ";
-    } else if (char === "\\") {
-      return "\\\\";
-    } else {
-      return char;
-    }
-  }).join("");
+  return p
+    .split("")
+    .map((char) => {
+      if (char === " ") {
+        return "\\ ";
+      } else if (char === "\\") {
+        return "\\\\";
+      } else {
+        return char;
+      }
+    })
+    .join("");
 }
 
 async function buildConfigTs() {
