0607b8c121d55266e8d8779063c4edef36c15f39
Duke Pan <matthewdukepan@gmail.com>
2024-08-23 18:54:26 -0400

    welcome page

--- extensions/vscode/src/commands.ts
+++ extensions/vscode/src/commands.ts
@@ -26,6 +26,7 @@ import { VerticalPerLineDiffManager } from "./diff/verticalPerLine/manager";
 import { QuickEdit, QuickEditShowParams } from "./quickEdit/QuickEditQuickPick";
 import { Battery } from "./util/battery";
 import type { VsCodeWebviewProtocol } from "./webviewProtocol";
+import { getExtensionUri } from "./util/vscode";
 
 let fullScreenPanel: vscode.WebviewPanel | undefined;
 
@@ -223,6 +224,14 @@ const commandsMap: (
   }
 
   return {
+    "pearai.openPearAiWelcome": async () => {
+      vscode.commands.executeCommand(
+        "markdown.showPreview",
+        vscode.Uri.file(
+          path.join(getExtensionUri().fsPath, "media", "welcome.md"),
+        ),
+      );
+    },
     "pearai.acceptDiff": async (newFilepath?: string | vscode.Uri) => {
       captureCommandTelemetry("acceptDiff");
 
